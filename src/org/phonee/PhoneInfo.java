d58a652af6d4009c5a1d5c8105ee311f17a24ae2
Preethyyy <preethisriram1802@gmail.com>
2022-11-24 09:20:02 -0800

    few lines coded

---
+++ src/org/phonee/PhoneInfo.java
@@ -0,0 +1,31 @@
+package org.phonee;
+
+public class PhoneInfo {
+     private void PhoneName() {
+	System.out.println("Redmi");
+     }
+	private void phoneMieiNum() {
+    System.out.println("0987654");
+	}
+	private void camera() {
+    System.out.println("64Mp");
+	}
+	private void Storage() {
+    System.out.println("128");
+	}
+	 
+	public static void main(String[] args) {
+		PhoneInfo d = new PhoneInfo();
+		d.PhoneName();
+		d.phoneMieiNum();
+		d.camera();
+		d.Storage();
+	}
+	
+	
+	
+	
+	
+	
+	
+}
